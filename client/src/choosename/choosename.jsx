ca18e9fbc82936a84f5a4334d61de5da3be0da76
Edlix <56119942+andal-x@users.noreply.github.com>
2020-04-20 20:13:19 +0200

    State inn textbox + one missing export

--- client/src/choosename/choosename.jsx
+++ client/src/choosename/choosename.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import PlaceholderNav from "../nav/nav";
-
+import Textb from "./textbox/textbox";
 class ChooseName extends Component {
   state = {};
   render() {
     return (
       <div>
         <h1>Create Game</h1>
+        <Textb/>
       </div>
     );
   }
--- client/src/choosename/textbox/textbox.jsx
+++ client/src/choosename/textbox/textbox.jsx
@@ -30,3 +30,4 @@ class Textb extends Component {
     );
     }
  }
+ export default Textb;
